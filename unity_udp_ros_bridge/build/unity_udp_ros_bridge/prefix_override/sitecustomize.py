c0f74c21cc6734b4eff3d3546f6fcb468cbce8dd
Jnorth2 <jar3dnorth51@gmail.com>
2026-05-28 22:48:54 -0700

    fixed git ignore

--- unity_udp_ros_bridge/build/unity_udp_ros_bridge/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/henry/Rover-Unity/unity_udp_ros_bridge/install/unity_udp_ros_bridge'
