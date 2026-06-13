df80f7c78de2f00cab080908a6e4625d7489071b
Nolan Kessler <kesslnol@oregonstate.edu>
2026-06-13 16:51:21 -0700

    Fixed Camera Feeds, enabled mulitcast

--- Assets/Control Interface App/CameraCanvas/UnityGstLaunch.cs
+++ Assets/Control Interface App/CameraCanvas/UnityGstLaunch.cs
@@ -2,24 +2,43 @@ using UnityEngine;
 using System.Diagnostics;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.UI;
+using UnityEditor.UI;
 
 public class GStreamerLauncher : MonoBehaviour
 {
     public string portNum;
-    public string sourcePort;
+    private string sourcePort;
+    private string sourceFramerate;
     private Process gStreamerProcess;
     public TMP_Dropdown dropdown;
 
-    private Dictionary<int, string> cameraPortMap = new Dictionary<int, string>()
+    private string mulitcastAddr = "239.0.0.1";
+
+    private class CameraEntry
     {
-        { 0, "42073" },
-        { 1, "42068" },
-        { 2, "42070" },
-        { 3, "42069" },
-        { 4, "42074" },
-        { 5, "42071" },
-        { 6, "42072" },
-        { 7, "42075" },
+        public string name;
+        public int port;
+        public string framerate;
+
+        public CameraEntry(string name, int port, string framerate)
+        {
+            this.name = name;
+            this.port = port;
+            this.framerate = framerate;
+        }
+    }
+
+    private Dictionary<int, CameraEntry> cameraPortMap = new Dictionary<int, CameraEntry>()
+    {
+        {0, new CameraEntry("IR",            42073, "30/1")},
+        {1, new CameraEntry("Tower",         42068, "30/1")},
+        {2, new CameraEntry("Chassis Right", 42070, "25/1")},
+        {3, new CameraEntry("Chassis Left",  42069, "25/1")},
+        {4, new CameraEntry("Gripper",       42074, "30/1")},
+        {5, new CameraEntry("Back",          42071, "25/1")},
+        {6, new CameraEntry("BEV",           42072, "25/1")},
+        {7, new CameraEntry("Chassis",       42075, "30/1")} //Not Used?
     };
 
     public int defaultCameraIndex = 3;
@@ -36,7 +55,8 @@ public class GStreamerLauncher : MonoBehaviour
 
         if (string.IsNullOrWhiteSpace(sourcePort) && cameraPortMap.ContainsKey(defaultCameraIndex))
         {
-            sourcePort = cameraPortMap[defaultCameraIndex];
+            sourcePort = cameraPortMap[defaultCameraIndex].port.ToString();
+            sourceFramerate = cameraPortMap[defaultCameraIndex].framerate;
         }
     }
 
@@ -45,7 +65,8 @@ public class GStreamerLauncher : MonoBehaviour
         if (cameraPortMap.ContainsKey(index))
         {
             StopGStreamer();
-            sourcePort = cameraPortMap[index];
+            sourcePort = cameraPortMap[index].port.ToString();
+            sourceFramerate = cameraPortMap[index].framerate;
             LaunchGStreamer();
         }
         else
@@ -77,11 +98,11 @@ public class GStreamerLauncher : MonoBehaviour
         ProcessStartInfo startInfo = new ProcessStartInfo();
         startInfo.FileName = "/bin/bash";
         // Decode the incoming UDP H265 stream and forward raw RGB frames to CameraListener over TCP.
-        startInfo.Arguments = "-c \"gst-launch-1.0 udpsrc port=" + sourcePort +
+        startInfo.Arguments = "-c \"gst-launch-1.0 udpsrc port=" + sourcePort + " address=" + mulitcastAddr +
             " caps=\\\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H265\\\" " +
             "! rtpulpfecdec ! rtpjitterbuffer latency=200 ! rtph265depay ! h265parse ! " +
             "queue max-size-buffers=3000 max-size-time=0 max-size-bytes=0 ! " +
-            "avdec_h265 ! videoconvert ! videorate ! video/x-raw,format=RGB,framerate=30/1 ! " +
+            "avdec_h265 ! videoconvert ! videorate ! video/x-raw,format=RGB,framerate=" + sourceFramerate + " ! " +
             "tcpclientsink host=127.0.0.1 port=" + portNum + " sync=false\"";
         startInfo.UseShellExecute = false;
         startInfo.RedirectStandardOutput = true;
